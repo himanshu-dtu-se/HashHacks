2c70fbf3dd58dc75bd90183f1677defde17681c2
Himanshu <Himanshu_bt2k16@dtu.ac.in>
2017-10-28 12:53:02 +0530

    More features

--- app/src/main/java/com/example/hashhacks/RecyclerAdapter.java
+++ app/src/main/java/com/example/hashhacks/RecyclerAdapter.java
@@ -1,6 +1,8 @@
 package com.example.hashhacks;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
@@ -8,8 +10,11 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -45,11 +50,13 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
                 .load(dataList.get(position).getUrl())
                 .into(holder.imageView);*/
         BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        options.inPreferredConfig = Bitmap.Config.ALPHA_8;
         Bitmap mustOpen = BitmapFactory.decodeFile(dataList.get(position).getUrl(), options);
         holder.imageView.setImageBitmap(mustOpen);
 
         holder.textView.setText(dataList.get(position).getTitle());
+
+
     }
 
     @Override
@@ -61,12 +68,69 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
 
         private ImageView imageView;
         private TextView textView;
+        private EditText editText;
+        private Button disapp, _app;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
             imageView = itemView.findViewById(R.id.thumbnail);
             textView = itemView.findViewById(R.id.title);
+            editText = itemView.findViewById(R.id.comments);
+            disapp = itemView.findViewById(R.id.not_eligible);
+
+            disapp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    new AlertDialog.Builder(context)
+                            .setMessage("Are you sure?? This will be marked as Not-Eligible.")
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(context, "Marked as Not-Eligible", Toast.LENGTH_LONG).show();
+                                    if(!TextUtils.isEmpty(editText.getText().toString())){
+                                        editText.setText("");
+                                    }
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            }).create().show();
+
+                }
+            });
+
+            _app = itemView.findViewById(R.id.approve);
+
+            _app.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    new AlertDialog.Builder(context)
+                            .setMessage("Are you sure?? This will be marked as Eligible.")
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(context, "Marked as Approved", Toast.LENGTH_LONG).show();
+                                    if(!TextUtils.isEmpty(editText.getText().toString())){
+                                        editText.setText("");
+                                    }
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            }).create().show();
+
+
+                }
+            });
         }
     }
 }
